0cd3f5927056c1f8185a51d5662073cde16d1845
rj matic <rjmatic19@gmail.com>
2024-07-28 01:33:51 +0800

    cet2

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+
 const ProtectedPage = () => {
+  const session = useSession();
+  useEffect(() => {
+    console.log(session);
+  }, [session]);
   return (
     <div className="w-full">
       <h1>Protected Content</h1>
